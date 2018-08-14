8f56b14b6dc7cde3864f86a776c42a8ebad55ceb
Liza Tennis and Nikki Wong <liza@>
2018-08-14 10:16:00 -0700

    add counting function with alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,22 @@
 $(document).ready(function() {
   $("#formOne").submit (function(event) {
     event.preventDefault();
-    var itemIds = ["item1", "item2", "item3", "item4", "item5", "item6"];
-    var answers = [];
-    itemIds.forEach(function(id) {
-      var answer = $("#" + id).val();
-      answers.push(answer);
-    });
 
-    answers.sort();
-    answers.forEach(function(lastanswer){
-      $("#shoppinglist").append("<li>" + lastanswer + "</li>");
-    });
+var countby = parseInt($("#countby").val());
+var countto = parseInt($("#countto").val());
 
+var total = 0;
+for(var index = countby; index <= countto; index += countby) {
+alert(index);
+  };
 
-
-
-
-
-
-    // $("#shoppinglist").append($("<li>").text(answers));
-
-    // display
-
-    // generate a list shopping list
-
-
-
-
-
-
-
-
-    $("#shoppinglist").show();
-    $("#formOne").hide();
+  // index.forEach(function(item){
+  //   $("#countresults").append("<li>" + item + "</li>");
+  //     $("#countresults").append(index);
+// });
+//
+//
+//     $("#countresults").show();
+//     $("#formOne").hide();
   });
 });
